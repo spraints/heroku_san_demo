dda1178b9c754d82d1c7d55c982db9a42ec2e3c9
Kenneth Mayer <kmayer@bitwrangler.com>
2011-06-21 08:36:14 -0700

    Initial commit
    # rails new heroku_san_demo -d sqlite3 -J -T

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HerokuSanDemo::Application.config.secret_token = '41c6ef642c033b038a21c14393c1bb79b0549e6ec0b8acf2343739193c88e49e0ae4851d78e858e43c72b2a3b424dbac01c82685d45209b80312393f22d53933'
